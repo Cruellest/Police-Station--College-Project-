62a3c21112613672d1ffd7b38a7284be08e3ff38
Cruellest <gabriel81523642@gmail.com>
2023-10-29 15:32:04 -0400

    Commented some parts of code and fixed a bug

--- codes/main.cpp
+++ codes/main.cpp
@@ -7,15 +7,19 @@ int main(){
     op = opção;
     */
     int op;
+
+    //This piece of code dynamicaly alocates all the files arrays and load the data from the files
     tViatura *viatura = (tViatura*) calloc(amountofViaturas(),sizeof(tViatura));
     loadViaturas(viatura);
     Policiais *policiais = (Policiais*) calloc(ammountofPoliceOfficers(),sizeof(Policiais));
     loadPoliceOfficers(policiais);
 
+    //Meu amor comenta aqui por favor te amo S2 <3
     tViaturaLogin *i, *f;
     i = NULL;   
     f = NULL;
     
+    //(WIP) UI for menu
     do{
 
         printf("\n1 - Viatura Login");
@@ -30,7 +34,9 @@ int main(){
 
         if(op == 1) //Viatura Login
         {
+            //loads functions from Viatura Login
             viaturaLogin(viatura, i, f);
+            //PFV EXPLICA ISSO TBM LINDA TE AMO
             for(tViaturaLogin *inicio = i; inicio->prox != NULL; inicio = inicio->prox){
                 printf("%d", inicio->viatura->codigo);
             }
@@ -63,6 +69,7 @@ int main(){
 
     }while(op != 0);
 
+    free(policiais);
     free(viatura);
     return 0;
 }
